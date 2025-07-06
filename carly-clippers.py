780eb9b73f2cf993230d1106c0e652ae85acaa88
tsaahr <lithy.10@hotmail.com>
2025-07-06 22:24:44 +0100

    fix: write error

--- carly-clippers.py
+++ carly-clippers.py
@@ -1,4 +1,4 @@
-"bowl", "bob", "mohawk", "flattop"]
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 total_price = 0
@@ -24,3 +24,4 @@ average_daily_revenue = total_revenue / 7
 cuts_under_30 = [hairstyles[i] for i in range(len(prices)) if prices[i] < 30]
 
 print(cuts_under_30)
+print("Cuts under $30:", cuts_under_30)
